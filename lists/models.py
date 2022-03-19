c01a0e6175a8a78a41b7c4d4d9909568e33d9381
cheezedoodles <hougvapes@gmail.com>
2022-03-19 19:36:52 +0300

    reworked lists

--- lists/models.py
+++ lists/models.py
@@ -1,11 +1,28 @@
-from django.db import models
+from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.db import models
 
 
 class List(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    shared_with = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='shared_lists'
+    )
 
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
+
+
+    @property
+    def name(self):
+        return self.item_set.first().text
+
+
+    @staticmethod
+    def create_new(first_item_text, owner=None):
+        list_ = List.objects.create(owner=owner)
+        Item.objects.create(text=first_item_text, list=list_)
+        return list_
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
 
-from lists.forms import ExistingListItemForm, ItemForm
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import List
-
 User = get_user_model()
+
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
+
 def new_list(request):
-    form = ItemForm(data=request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List()
-        list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
+    return render(request, 'home.html', {'form': form})
+
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -30,6 +28,14 @@ def view_list(request, list_id):
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
 
+
 def my_lists(request, email):
     owner = User.objects.get(email=email)
     return render(request, 'my_lists.html', {'owner': owner})
+
+
+def share_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    list_.shared_with.add(request.POST['sharee'])
+    return redirect(list_)
+
