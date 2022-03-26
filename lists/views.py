ac97679a856d51bf3234d858f0dc63a9a4030f60
cheezedoodles <hougvapes@gmail.com>
2022-03-26 18:27:47 +0300

    home_page view is now a class

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
+from django.views.generic import FormView
 
 from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import List
 User = get_user_model()
-
-
-def home_page(request):
-    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def new_list(request):
@@ -39,3 +36,6 @@ def share_list(request, list_id):
     list_.shared_with.add(request.POST['sharee'])
     return redirect(list_)
 
+class HomePageView(FormView):
+    template_name = 'home.html'
+    form_class = ItemForm
